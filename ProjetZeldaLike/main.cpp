2bfe5a42e9d51d8646f4e58e352d7a5e019ecadc
AmadeoPonceTro2Ouf <aponce@gaming.tech>
2025-01-29 10:31:46 +0100

    include sfml

---
+++ ProjetZeldaLike/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+#include "Entity.h"
+
+
+using namespace std;
+using namespace sf;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << " test git ";
+}
+
+
