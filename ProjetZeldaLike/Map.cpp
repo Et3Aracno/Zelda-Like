63044f6655ab5582dba69e8bb0e298b1f1997470
Et3Aracno <luhatmaxime@gmail.com>
2025-01-30 12:16:55 +0100

    add map to map branch

---
+++ ProjetZeldaLike/Map.cpp
@@ -0,0 +1,93 @@
+#include "Map.h"
+void Map::initM(){
+
+}
+void Map::DrawM(){
+
+}
+void Map::initArbre(){
+
+}
+void Map::initSext(){
+
+}
+void Map::initTour(){
+
+}
+void Map::initPnj(){
+
+}
+void Map::initTombe(){
+
+}
+void Map::initChemin(){
+
+}
+void Map::initJ(){
+
+}
+void Map::initMu(){
+
+}
+void Map::initS(){
+
+}
+void Map::initP(){
+
+}
+void Map::initKey(){
+
+}
+void Map::initM() {
+	window = new RenderWindow(VideoMode(1920, 1080), "Steam Purgator");
+	ifstream file(fileM);
+	if (!file.is_open()) {
+		cout << endl << "Impossible de lire le fichier txt" << fileM << endl;
+	}
+	string line;
+	while (getline(file, line)) {
+		vM.push_back(line);
+	}
+}
+void Map::DrawM() {
+	float Width = static_cast<float>(window->getSize().x) / vM[0].size();
+	float Height = static_cast<float>(window->getSize().y) / vM.size();
+	for (size_t i = 0; i < vM.size(); i++) {
+		for (size_t j = 0; j < vM[0].size(); j++) {
+			switch (vM[i][j])
+			{
+			case '#':
+			{
+				RectangleShape arbre(Vector2f(80, 99));
+				arbre.setPosition(Vector2f(80 * j, 99 * i));
+				arbre.setTexture(&txtArbre);
+				arbre.setPosition(j * Width, i * Height);
+				vMur.push_back(arbre);
+				break;
+			}
+
+			case'.':
+			{
+				RectangleShape sol(Vector2f(80, 99));
+				sol.setPosition(Vector2f(80 * j, 99 * i));
+				sol.setTexture(&txtS);
+				sol.setPosition(j * Width, i * Height);
+				vSol.push_back(sol);
+				break;
+			}
+
+			case'D':
+			{
+				RectangleShape porte(Vector2f(80, 99));
+				porte.setPosition(Vector2f(80 * j, 99 * i));
+				porte.setTexture(&txtP);
+				porte.setPosition(j * Width, i * Height);
+				vPorte.push_back(porte);
+				break;
+			}
+			default:
+				break;
+			}
+		}
+	}
+}
---
+++ ProjetZeldaLike/Map.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <thread>
+#include <vector>
+#include<fstream>
+#include<sstream>
+using namespace std;
+using namespace sf;
+
+
+
+class Map{
+protected:
+	string fileM;
+	
+public:
+	void initM();
+	void DrawM();
+	void initArbre();
+	void initSext();
+	void initTour();
+	void initPnj();
+	void initTombe();
+	void initChemin();
+	void initJ();
+	void initMu();
+	void initS();
+	void initP();
+	void initKey();
+	void initall();
+
+
+	RenderWindow* window;//ajouter ça dans le game pour appeler la window et commencer la boucle de jeux  Map map; /*map.initM(); while (map.window) {}*/
+	Texture txtArbre,txtSext,txtTour,txtPnj,txtTombe,txtChemin,txtJ, txtMu, txtS,txtP, txtKey;
+	Sprite sprtJ, sprtMu, sprtS, sprtP, sprtKey,sprtArbre,sprtSext,sprtPnj,sprtTomb,sprtChemin;
+	vector<string>vM;
+	vector<RectangleShape>vMur;
+	vector<RectangleShape>vSol;
+	vector<RectangleShape>vPorte;
+
+	
+};
